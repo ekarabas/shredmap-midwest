e6e556f04d68f1da2d4a0e5428f2c83fedafd9f2
ekarabas <ekarabas@wisc.edu>
2022-02-22 19:37:57 -0600

    Update procfile to new wsgi route

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['shredmap-midwest.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'shredmap-midwest.herokuapp.com/']
 
 
 # Application definition

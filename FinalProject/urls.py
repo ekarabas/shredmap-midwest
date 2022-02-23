37a08af7dd001633b4bb993c8ea3e46b52aad416
ekarabas <ekarabas@wisc.edu>
2022-02-23 13:03:07 -0600

    Took media root out of urls

--- FinalProject/urls.py
+++ FinalProject/urls.py
@@ -22,8 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("shredmap_midwest.urls")),
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root':       settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,
         {'document_root': settings.STATIC_ROOT}),
 ]

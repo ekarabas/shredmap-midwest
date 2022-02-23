5fce43b64b86c46e44b8826b3b310b96374d8709
ekarabas <ekarabas@wisc.edu>
2022-02-23 14:37:34 -0600

    Change url to re_path

--- FinalProject/urls.py
+++ FinalProject/urls.py
@@ -16,14 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
-from django.conf.urls import url
+from django.conf.urls import re_path
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("shredmap_midwest.urls")),
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root':       settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT}),
 ]

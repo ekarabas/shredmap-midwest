431c9453b6fdd2ca63f60b8b8c99e3303628ce29
ekarabas <ekarabas@wisc.edu>
2022-02-22 18:59:06 -0600

    Update requirements and settings for .env

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^t$rq9tg7l9hj&pec3tdp!hq(jo@&)7=!y(7p_0pt64jv!$!&b'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

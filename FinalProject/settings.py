eec8750a5029f928cf0353dfc5f1769ccee7b427
ekarabas <ekarabas@wisc.edu>
2022-02-22 18:36:59 -0600

    Get ready for Django deployment

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^t$rq9tg7l9hj&pec3tdp!hq(jo@&)7=!y(7p_0pt64jv!$!&b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
